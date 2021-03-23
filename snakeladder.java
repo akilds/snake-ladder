3bdc1571fac322118bfc578d1353354d0b06116d
akilds <akilrainadesigan@gmail.com>
2021-03-23 12:34:58 +0530

    Two Players

--- snakeladder.java
+++ snakeladder.java
@@ -2,37 +2,93 @@ import java.util.*;
 
 public class snakeladder
 {
- public static void main(String[] args)
+ static int die_roll(int pos,int playr)
  {
-  int position = 0;
-  int die_count = 0;
-  System.out.println("Start Position : " + position);
-  Random random = new Random();
-  while(position<100)
-  {
-   int roll = random.nextInt(5)+1;
-   int check = random.nextInt(3);
-   die_count++;
+  int position = pos;
+  int player = playr;
+  Random rand = new Random();
+  int roll = rand.nextInt(5)+1;
+  int check = rand.nextInt(3);
    switch(check)
    {
-    case 0 : System.out.println("Ladder");
+    case 0 : System.out.println("Ladder for player" + player);
              position += roll;
              if(position>100)
              {
               position -= roll;
              }
-             break;
-    case 1 : System.out.println("Snake");
+             System.out.println("Current position : " + position);
+             position = die_roll(position,player);
+             return position;
+    case 1 : System.out.println("Snake for player" + player);
              position -= roll;
              if(position<0)
              {
                position = 0;
              }
-             break;
-    default : System.out.println("No Play");
+             System.out.println("Current position : " + position);
+             return position;
+    default : System.out.println("No Play for player" + player);
+              System.out.println("Current position : " + position);
+              return position;
    }
-   System.out.println("Current position : " + position);
+ }
+ public static void main(String[] args)
+ {
+  int position_player1 = 0, position_player2 = 0;
+  Random random = new Random();
+  while(position_player1<100 && position_player2<100)
+  {
+   int roll_player1 = random.nextInt(5)+1;
+   int check_player1 = random.nextInt(3);
+   switch(check_player1)
+   {
+    case 0 : System.out.println("Ladder for player1");
+             position_player1 += roll_player1;
+             if(position_player1>100)
+             {
+              position_player1 -= roll_player1;
+             }
+             position_player1 = die_roll(position_player1,1);
+             break;
+    case 1 : System.out.println("Snake for player1");
+             position_player1 -= roll_player1;
+             if(position_player1<0)
+             {
+               position_player1 = 0;
+             }
+             break;
+    default : System.out.println("No Play for player1");
+   }
+   int roll_player2 = random.nextInt(5)+1;
+   int check_player2 = random.nextInt(3);
+   switch(check_player2)
+   {
+    case 0 : System.out.println("Ladder for player2");
+             position_player2 += roll_player2;
+             if(position_player2>100)
+             {
+              position_player2 -= roll_player2;
+             }
+             position_player2 = die_roll(position_player2,2);
+             break;
+    case 1 : System.out.println("Snake for player2");
+             position_player2 -= roll_player2;
+             if(position_player2<0)
+             {
+               position_player2 = 0;
+             }
+             break;
+    default : System.out.println("No Play for player2");
+   }
   }
-  System.out.println("Number of times dice rolled : " + die_count);
+  if(position_player1==100)
+  {
+   System.out.println("Player1 Wins");
+  }
+  else
+  {
+   System.out.println("Player2 Wins");
+  }
  }
 }
