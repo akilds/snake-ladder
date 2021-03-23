6c87057c94b98adc5a763d30f8a92a02f57533d4
akilds <akilrainadesigan@gmail.com>
2021-03-23 12:41:17 +0530

    Winning Position

--- snakeladder.java
+++ snakeladder.java
@@ -7,10 +7,9 @@ public class snakeladder
   int position = 0;
   System.out.println("Start Position : " + position);
   Random random = new Random();
-  int roll = random.nextInt(5)+1;
-  System.out.println("Number on the Die : " + roll);
   while(position<=100)
   {
+   int roll = random.nextInt(5)+1;
    int check = random.nextInt(3);
    switch(check)
    {
