c3ba9f1799208b873ff9da507d4759798973c426
akilds <akilrainadesigan@gmail.com>
2021-03-23 11:22:04 +0530

    Play Check

--- snakeladder.java
+++ snakeladder.java
@@ -9,5 +9,17 @@ public class snakeladder
   Random random = new Random();
   int roll = random.nextInt(5)+1;
   System.out.println("Number on the Die : " + roll);
+  int check = random.nextInt(3);
+  switch(check)
+  {
+   case 0 : System.out.println("Ladder");
+            position += roll;
+            break;
+   case 1 : System.out.println("Snake");
+            position -= roll;
+            break;
+   default : System.out.println("No Play");
+  }
+  System.out.println("Current position : " + position);
  }
 }
