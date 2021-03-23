4d18f5858a7cad160443b65aa35d3e2060de7345
akilds <akilrainadesigan@gmail.com>
2021-03-23 11:10:30 +0530

    Die Roll

--- snakeladder.java
+++ snakeladder.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class snakeladder
 {
  public static void main(String[] args)
  {
   int position = 0;
   System.out.println("Start Position : " + position);
+  Random random = new Random();
+  int roll = random.nextInt(5)+1;
+  System.out.println("Number on the Die : " + roll);
  }
 }
