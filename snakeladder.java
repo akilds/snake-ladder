8a4baf49ca5bc2937afbf951157bd42a15688a11
akilds <akilrainadesigan@gmail.com>
2021-03-23 11:43:37 +0530

    Report

--- snakeladder.java
+++ snakeladder.java
@@ -5,12 +5,14 @@ public class snakeladder
  public static void main(String[] args)
  {
   int position = 0;
+  int die_count = 0;
   System.out.println("Start Position : " + position);
   Random random = new Random();
   while(position<100)
   {
    int roll = random.nextInt(5)+1;
    int check = random.nextInt(3);
+   die_count++;
    switch(check)
    {
     case 0 : System.out.println("Ladder");
@@ -31,5 +33,6 @@ public class snakeladder
    }
    System.out.println("Current position : " + position);
   }
+  System.out.println("Number of times dice rolled : " + die_count);
  }
 }
